a44992b7a012e1b5be37be701d4fb9290fe57895
Jason <arrojason@gmail.com>
2021-05-27 19:15:47 +1000

    Zoo App

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour
+{
+    public Text Title,description, exhibit;
+    public Image animalImage;
+
+    public List<CardModel> cards ;
+
+
+
+    public void CallCard(int value)
+    {
+        Title.text = cards[value].Title;
+        description.text = cards[value].Description;
+        exhibit.text = cards[value].Exhibit;
+        animalImage.sprite = cards[value].AnimalImage;
+    }
+}
